ff0309b1dad9dead7a4e4a537890205546ac535c
Bret Leasure <bret.leasure@outlook.com>
2018-04-09 15:57:37 -0500

    Initial Commit

---
+++ CAP.Apps.PlaceComponentPlus/CAP.Apps.PlaceComponentPlus/PlaceOpenCompUI.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CAP.Apps.PlaceComponentPlus
+{
+    public partial class PlaceOpenCompUI : Form
+    {
+        public PlaceOpenCompUI()
+        {
+            InitializeComponent();
+        }
+
+        List<InvDoc> OpenComps;
+
+        string PlaceCompFilepath;
+
+        private void PlaceComponentUI_Load(object sender, EventArgs e)
+        {
+            OpenComps = ApplicationTools.GetOpenDocuments();
+
+            PopulateListBox();
+
+        }
+
+        void PopulateListBox()
+        {
+            foreach (InvDoc item in OpenComps)
+            {
+                lb_OpenDocs.Items.Add(item.Name);
+            }
+        }
+
+        private void btn_Place_Click(object sender, EventArgs e)
+        {
+            if (lb_OpenDocs.SelectedItem != null)
+            {
+                string compName = lb_OpenDocs.SelectedItem.ToString();
+
+                foreach (InvDoc item in OpenComps)
+                {
+                    if (item.Name == compName)
+                    {
+                        AddinGlobal.PlaceCompFilePath = item.Filepath;
+                        this.Close();
+                        break;
+                    }
+                }
+            }
+            
+            
+        }
+    }
+}
